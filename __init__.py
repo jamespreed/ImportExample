694ef255e7540973da419bdc62b09c06d8c335e5
jamespreed <jamesreed.physics@gmail.com>
2018-11-06 06:44:32 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# The __init__ of the top level directory allows us to populate the
+# namespace with the objects we want to share to siblings
+from .path1.inner1 import hello
---
+++ path2/inner2.py
@@ -0,0 +1,7 @@
+# import the object `hello` from the top level 
+from importtest import hello 
+# this would also work: 
+# from .. import hello
+
+def world():
+    return 'world'
